00dfb48ee4fee60ddb514b2a09779f06ec15e7e2
21shield <21netaly@gmail.com>
2020-10-24 15:10:26 -0400

    Modify: frequency.js

---
+++ patterns/frequency.js
@@ -0,0 +1,7 @@
+//  Problem:
+// Write a funciton called same which accepts two arrays. The function should return true if every value in the array has its corresponding value squared in the second array. The frequency of values most be the same. 
+    // Examples:
+        // same([1,2,3], [4,1,9]) => true
+        // same([1,2,3], [1,9]) => false
+        // same([1,2,1], [4,4,1]) => false (must be same frequency)
+    
