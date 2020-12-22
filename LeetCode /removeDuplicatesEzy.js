37abbba483ff08f3e146b809d7ae5b5bb0e2abda
21shield <21netaly@gmail.com>
2020-12-22 14:05:00 -0500

    Created new folder for algo problems

---
+++ LeetCode /removeDuplicatesEzy.js
@@ -0,0 +1,25 @@
+// Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+// Example 1:
+
+// Input: 1->1->2
+// Output: 1->2
+// Example 2:
+
+// Input: 1->1->2->3->3
+// Output: 1->2->3
+// ````` funciton below`````
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+// /**
+// //  @param {ListNode} head
+// // @return {ListNode}
+
+
+var deleteDuplicates = function(head) {
+    
+}
