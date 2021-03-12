4bb6d4c92b5b2f8d744db12a2ae583cc121ca288
PeaWarrior <jacksonchen171@gmail.com>
2021-03-12 13:11:32 -0500

    initialize bst

---
+++ data-structures/binarysearchtree.js
@@ -0,0 +1,4 @@
+// Starting from the root node, the root node branches out like a tree.
+// At most, binary trees only have 2 children per node. Hence, binary.
+// Every node can only point to a child and is moving away from the root node.
+// Each node to the left is less than the parent and every node to the right is greater.
